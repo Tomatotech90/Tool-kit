3eb940a139b6aafe8f7a823baf353fb3cec50ef5
Ket_ch0p <96146875+Tomatotech90@users.noreply.github.com>
2022-11-05 15:30:49 -0400

    Create nm4p_IDSFrecon.sh

---
+++ nm4p_IDSFrecon.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#Nmap recon
+#interact script is base ont eh results
+#feel free to change the script
+#-----------------------------------------------------------------------------------
+
+ echo "First check who is in the other side"
+
+ echo " Target Ip: "
+ read Target
+
+ echo " Wafw00f "
+ echo " "
+ echo "------------------"
+ wafw00f -a $Target -v
+ echo "------------------"
+
+#-----------------------------------------------------------------------------------
+
+ echo "Nmap scanning"
+ echo " using D (decoy) and RND (random) "
+ echo " "
+#changet the flags if you have a decoy
+#you can choo how many random ips exam: RND:#
+ sudo nmap -sS -sV  -D RND: $Target
+
+ echo " "
+#----------------------------------------------------------------------------------
+#the mtu is the size of the package, base on 8,16....
+ echo "fragment teh packages"
+ echo "the size of mtu: "
+
+ read size
+
+  sudo nmap -sS -sV --mtu $size -D  $Target
+
+ echo " "
+#------------------------------------------------------------------------------------
+
+ echo " Banner "
+ echo " "
+ echo "will perform two commands, if you want you comment one "
+ echo " "
+ echo "the command is the following: sudo nmap -sS -sV  -D RND: $Target"
+ echo " "
+ echo "sudo nmap -sS -sV --mtu $size -D  $Target "
+ 
+ #base on the results you need to change this
+ sudo nmap -F -T4 --script banner  -D RND: $Target
+ echo " "
+ echo " "
+ sudo nmap -F -T4 --script banner --mtu $size -D  $Target
+  
+ echo "Done"
