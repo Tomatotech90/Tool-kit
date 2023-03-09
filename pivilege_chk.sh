22168385b60451604ed06ddb174c3efd4af68b6a
Ket_ch0p <96146875+Tomatotech90@users.noreply.github.com>
2023-03-08 21:48:09 -0500

    Update pivilege_chk.sh

--- pivilege_chk.sh
+++ pivilege_chk.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Check if running as root
 if [ "$(id -u)" -eq 0 ]; then
   echo "Running as root"
@@ -22,3 +24,36 @@ if [ -f "/etc/sudoers" ]; then
   echo "Checking for misconfigured sudoers file..."
   visudo -c
 fi
+
+# Check sudo version
+echo "Checking sudo version..."
+sudo -V
+
+# Check for known sudo vulnerabilities
+echo "Checking for known sudo vulnerabilities..."
+searchsploit sudo
+
+# Check for recent CVEs
+echo "Checking for recent CVEs..."
+sudo apt-get update > /dev/null
+sudo apt-get upgrade -s | grep -Eo 'CVE-[0-9]{4}-[0-9]{4}' | sort -u
+
+# Check for sudoedit vulnerabilities
+echo "Checking for sudoedit vulnerabilities..."
+sudoedit -s '\' `perl -e 'print "A"x100'`
+
+# Check for sudo privilege escalation vulnerabilities
+echo "Checking for sudo privilege escalation vulnerabilities..."
+sudo -l
+
+# Check for sudo pty vulnerabilities
+echo "Checking for sudo pty vulnerabilities..."
+sudo sh -c 'echo A | sudo -S su' 2>/dev/null
+
+# Check for sudo heap overflow vulnerabilities
+echo "Checking for sudo heap overflow vulnerabilities..."
+sudoedit -s '\' `perl -e 'print "A"x40000'`
+
+# Check for sudo format string vulnerabilities
+echo "Checking for sudo format string vulnerabilities..."
+sudoedit -s '\' `printf "%s\n" "%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n%n
