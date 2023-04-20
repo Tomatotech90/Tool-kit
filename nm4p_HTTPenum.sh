c9cd5b0c12e92d52261f23acc31d0b96c6f01140
Ket_ch0p <96146875+Tomatotech90@users.noreply.github.com>
2023-04-19 20:22:41 -0400

    Update nm4p_HTTPenum.sh

--- nm4p_HTTPenum.sh
+++ nm4p_HTTPenum.sh
@@ -4,26 +4,28 @@
 #interact script is base on the results
 #feel free to change the script
 #-----------------------------------------------------------------------------------
- echo "WELCOME"
- 
- echo" "
- 
-#-----------------------------------------------------------------------------------
- echo " Target Ip: "
- read Target
-#------------------------------------------------------------------------------------
- #quick scan to check port 80
- echo "Nmap will do a recon scan"
- 
- sudo nmap -F -sV -T5 $Target
- 
-#-------------------------------------------------------------------------------------
- echo "Nmap will check port 80"
- echo " "
- echo "using scritp http-enum"
 
- sudo nmap -sV -p 80 --script http-enum $Target
- #------------------------------------------------------------------------------------
- 
- 
- 
+echo "WELCOME"
+
+echo " "
+
+#-----------------------------------------------------------------------------------
+echo "Target Ip: "
+read Target
+
+#------------------------------------------------------------------------------------
+#quick scan to check port 80
+echo "Nmap will do a recon scan"
+
+sudo nmap -F -sV -T5 "$Target"
+
+#-------------------------------------------------------------------------------------
+http_enum_script="http-enum"
+
+echo "Nmap will check port 80"
+echo " "
+echo "using script $http_enum_script"
+
+sudo nmap -sV -p 80 --script "$http_enum_script" "$Target"
+
+#------------------------------------------------------------------------------------
